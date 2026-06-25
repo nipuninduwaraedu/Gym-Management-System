60a99ae162068268e6de5ec55598ce287c33e088
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-25 18:34:39 +0530

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const connectDB = require("./src/config/db");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
