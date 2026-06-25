a705e1d731688896769667c53da4018b965d666f
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-25 18:53:30 +0530

    create user register controller and route

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,50 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    console.log("Register API Hit");
+
+    const { name, email, password } = req.body;
+
+    console.log(req.body);
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
