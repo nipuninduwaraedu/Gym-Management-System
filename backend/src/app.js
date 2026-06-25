e76f6c357d12b790b3ed587e005ceb8ee05d9501
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-25 23:02:53 +0530

    add user route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
